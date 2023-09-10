5f1459bda85ba7ed538cb791b2beffa586b56d03
Nader Sayed <nadersayed001@gmail.com>
2023-09-10 14:44:08 +0300

    [Create] Dish Model Without Realtions

---
+++ Models/Dish.cs
@@ -0,0 +1,14 @@
+﻿namespace Resturant_RES_API_ITI_PRJ.Models
+{
+    public class Dish
+    {
+        public int DishId { get; set; }
+
+        public string DishName { get; set; }
+
+        public double DishPrice { get; set; }
+
+        public string DishImageName { get; set; }
+
+    }
+}
