f96dc96123a1fa818811a06620bd6c7272f4097d
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-09-10 13:34:28 +0300

    [Add] Custom Validations

--- Validations/Class.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Resturant_RES_API_ITI_PRJ.Validations
-{
-    public class Class
-    {
-    }
-}
---
+++ Validations/CustomMinDateAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.DataAnnotation
+{
+    public class CustomMinDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime dateValue)
+            {
+                return dateValue >= DateTime.Today;
+            }
+            return false;
+        }
+    }
+}
