0ccb9b68e2519f8896f0e22aca03089693909762
Nader Sayed <nadersayed001@gmail.com>
2023-09-10 14:33:00 +0300

    [Create] Customer Addresses Models without Realtions Props

---
+++ Models/CustomerAddresses.cs
@@ -0,0 +1,18 @@
+﻿namespace Resturant_RES_API_ITI_PRJ.Models
+{
+    public class CustomerAddresses
+    {
+        public int CustAddressId { get; set; }
+
+        public string? CustAddressBuildingNumber { get; set; }
+
+        public string CustAddressStreet { get; set; }
+
+        public string CustAddressCity { get; set; }
+
+        public string CustAddressCountry { get; set; }
+
+        public string? CustAddressPostalCode { get; set; }
+
+    }
+}
