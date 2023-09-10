d41051391639e270fef4290593d10a2df66872ec
Nader Sayed <nadersayed001@gmail.com>
2023-09-10 14:52:50 +0300

    [Create] Dish Category Model Without Realtion

---
+++ Models/DishCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Resturant_RES_API_ITI_PRJ.Models
+{
+    public class DishCategory
+    {
+        public int DishCategoryId { get; set; }
+
+        public string DishCategoryName { get; set; }
+    }
+}
