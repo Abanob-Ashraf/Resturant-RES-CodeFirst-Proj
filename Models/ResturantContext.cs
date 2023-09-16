d4fafb853fde0065f243936e01d4392a637de793
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-09-16 04:25:54 +0300

    [Implement] Resturant Db context with fluent API for Order schema

--- Models/ResturantContext.cs
+++ Models/ResturantContext.cs
@@ -22,6 +22,13 @@ namespace Resturant_RES_API_ITI_PRJ.Models
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<DishIngredientRel> DishIngredientRels { get; set; }
 
+        // Order
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderState> OrderStates { get; set; }
+        public DbSet<OrderType> OrderTypes { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<OrderDishesRel> OrderDishesRels { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Management
@@ -70,17 +77,22 @@ namespace Resturant_RES_API_ITI_PRJ.Models
                     .HasComment("الرقم القومي");
 
                 entity.Property(e => e.EmpHirigDate)
+                    .IsRequired(true)
                     .HasColumnType("datetime2")
+                    .HasDefaultValueSql("GETDATE()")
                     .HasComment("تاريخ التعيين");
 
                 entity.Property(e => e.EmpSalary)
+                    .IsRequired(true)
                     .HasColumnType("decimal")
                     .HasComment("المرتب");
 
                 entity.Property(e => e.FranchiseId)
+                    .IsRequired(true)
                     .HasComment("كود الفرع");
 
                 entity.Property(e => e.EmpCategoryId)
+                    .IsRequired(true)
                     .HasComment("كود الوظيفه");
 
                 entity.HasIndex(e => e.EmpEmail).IsUnique(true);
@@ -128,9 +140,11 @@ namespace Resturant_RES_API_ITI_PRJ.Models
                     .HasMaxLength(50)
                     .IsRequired(true)
                     .IsUnicode(false)
+                    //.HasDefaultValueSql("'Egypt'")
                     .HasComment("الدوله");
 
                 entity.Property(e => e.EmpId)
+                    .IsRequired(true)
                     .HasComment("كود الموظف");
 
                 entity.HasOne(e => e.EmployeeIdNavigation)
@@ -164,12 +178,12 @@ namespace Resturant_RES_API_ITI_PRJ.Models
                 entity.Property(e => e.Country)
                     .HasMaxLength(50)
                     .IsRequired(true)
+                    //.HasDefaultValueSql("'Egypt'")
                     .IsUnicode(false)
                     .HasComment("الدوله");
 
                 entity.Property(e => e.ManagerId)
                     .IsRequired(false)
-                    .IsUnicode(false)
                     .HasComment("كود المدير");
 
                 entity.HasOne(e => e.ManagerIdNavigation)
@@ -224,6 +238,7 @@ namespace Resturant_RES_API_ITI_PRJ.Models
                     .HasComment("صوره الطبق");
 
                 entity.Property(e => e.DishCategoryId)
+                    .IsRequired(true)
                     .HasComment("كود التصنيف");
 
                 entity.HasOne(e => e.DishCategoryIdNavigation)
@@ -240,6 +255,7 @@ namespace Resturant_RES_API_ITI_PRJ.Models
                 entity.ToTable("dishCategory", "dish");
 
                 entity.Property(e => e.DishCategoryId)
+                    .IsRequired(true)
                     .HasComment("كود الصنف");
 
                 entity.Property(e => e.DishCategoryName)
@@ -256,7 +272,9 @@ namespace Resturant_RES_API_ITI_PRJ.Models
 
                 entity.ToTable("ingredient", "dish");
 
-                entity.Property(e => e.IngredientId).HasComment("كود المكونات");
+                entity.Property(e => e.IngredientId)
+                    .IsRequired(true)
+                    .HasComment("كود المكونات");
 
                 entity.Property(e => e.IngredientName)
                     .HasMaxLength(50)
@@ -271,24 +289,194 @@ namespace Resturant_RES_API_ITI_PRJ.Models
 
                 entity.ToTable("dishIngredient", "dish");
 
-                entity.Property(e => e.DishIngredientRelId).HasComment("كود الطبق و المكون");
+                entity.Property(e => e.DishIngredientRelId)
+                .HasComment("كود الطبق و المكون");
 
-                entity.Property(e => e.DishId).HasComment("كود الطبق");
+                entity.Property(e => e.DishId)
+                    .IsRequired(true)
+                    .HasComment("كود الطبق");
 
-                entity.Property(e => e.IngredientId).HasComment("كود المكون");
+                entity.Property(e => e.IngredientId)
+                    .IsRequired(true)
+                    .HasComment("كود المكون");
 
                 entity.HasOne(e => e.DishIdNavigation)
                     .WithMany(c => c.DishIngredientRels)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_dishIngredient_dish");
-                
+
                 entity.HasOne(e => e.IngredientIdNavigation)
                     .WithMany(c => c.DishIngredientRels)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_dishIngredient_ingredient");
             });
 
+            // Order
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(e => e.OrderId);
 
+                entity.ToTable("order", "order");
+
+                entity.Property(e => e.OrderId).HasComment("كود الطلب");
+
+                entity.Property(e => e.OrderSerialNum)
+                    .HasMaxLength(50)
+                    .IsRequired(true)
+                    .IsUnicode(false)
+                    .HasComment("رقم الطلب");
+
+                entity.Property(e => e.IsPaid)
+                    .IsRequired(true)
+                    .HasDefaultValueSql("0")
+                    .HasComment("هل تم الدفع؟");
+
+                entity.Property(e => e.OrderDate)
+                    .IsRequired(true)
+                    .HasColumnType("datetime2")
+                    .HasDefaultValueSql("GETDATE()")
+                    .HasComment("تاريخ الطلب");
+
+                entity.Property(e => e.OrderTypeId)
+                    .IsRequired(true)
+                    .HasComment("كود نوع الطلب");
+
+                entity.Property(e => e.OrderStateId)
+                    .IsRequired(true)
+                    .HasComment("كود حاله الطلب");
+
+                entity.Property(e => e.PaymentMethodId)
+                    .IsRequired(true)
+                    .HasComment("طريقه الدفع");
+
+                entity.Property(e => e.CustomerId)
+                    .IsRequired(true)
+                    .HasComment("كود العميل");
+
+                entity.Property(e => e.FranchiseId)
+                    .IsRequired(true)
+                    .HasComment("كود الفرع");
+
+                entity.HasIndex(e => e.OrderSerialNum).IsUnique(true);
+
+                entity.HasOne(e => e.OrderTypeIdNavigation)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(e => e.OrderTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_order_orderType");
+
+                entity.HasOne(e => e.OrderStateIdNavigation)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(e => e.OrderStateId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_order_orderState");
+
+                entity.HasOne(e => e.PaymentMethodIdNavigation)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(e => e.PaymentMethodId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_order_paymentMethod");
+
+                entity.HasOne(e => e.CustomerIdNavigation)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(e => e.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_order_customer");
+
+                entity.HasOne(e => e.FranchiseIdNavigation)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(e => e.FranchiseId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_order_Franchise");
+            });
+
+            modelBuilder.Entity<OrderType>(entity =>
+            {
+                entity.HasKey(e => e.OrderTypeId);
+
+                entity.ToTable("orderType", "order");
+
+                entity.Property(e => e.OrderTypeId)
+                    .IsRequired(true)
+                    .HasComment("كود نوع الطلب");
+
+                entity.Property(e => e.OrderTypeName)
+                    .IsRequired(true)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasComment("اسم نوع الطلب");
+            });
+
+            modelBuilder.Entity<OrderState>(entity =>
+            {
+                entity.HasKey(e => e.OrderStateId);
+
+                entity.ToTable("orderState", "order");
+
+                entity.Property(e => e.OrderStateId)
+                    .IsRequired(true)
+                    .HasComment("كود حاله الطلب");
+
+                entity.Property(e => e.OrderStateName)
+                    .IsRequired(true)
+                    .IsUnicode(false)
+                    .HasMaxLength(50)
+                    .HasComment("اسم حاله الطلب");
+            });
+
+            modelBuilder.Entity<PaymentMethod>(entity =>
+            {
+                entity.HasKey(e => e.PaymentMethodId);
+
+                entity.ToTable("paymentMethod", "order");
+
+                entity.Property(e => e.PaymentMethodId)
+                    .IsRequired(true)
+                    .HasComment("كود طريقه الدفع");
+
+                entity.Property(e => e.PaymentMethodName)
+                    .IsRequired(true)
+                    .IsUnicode(false)
+                    .HasMaxLength(50)
+                    .HasComment("اسم طريقه الدفع");
+            });
+
+            modelBuilder.Entity<OrderDishesRel>(entity =>
+            {
+                entity.HasKey(e => e.OrderDishesRelId);
+
+                entity.ToTable("orderDishesRel", "order");
+
+                entity.Property(e => e.OrderDishesRelId)
+                    .IsRequired(true)
+                    .HasComment("كود الطلب والطبق");
+
+                entity.Property(e => e.Quantity)
+                    .HasColumnType("integer")
+                    .IsUnicode(false)
+                    .IsRequired(true)
+                    .HasComment("الكميه");
+
+                entity.Property(e => e.OrderId)
+                    .IsRequired(true)
+                    .HasComment("كود الطلب");
+
+                entity.Property(e => e.DishId)
+                    .IsRequired(true)
+                    .HasComment("كود الطبق");
+
+                entity.HasOne(e => e.OrderIdNavigation)
+                    .WithMany(c => c.OrderDishesRels)
+                    .HasForeignKey(e => e.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_orderDishesRel_order");
+
+                entity.HasOne(e => e.DishIdNavigation)
+                    .WithMany(c => c.OrderesDishesRels)
+                    .HasForeignKey(e => e.DishId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_orderDishesRel_dish");
+            });
         }
     }
 }
